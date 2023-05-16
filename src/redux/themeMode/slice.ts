2a1c68ec77e7ee1e6c972c05f618f0ca75646298
Pavel Zolotin <zolotinpavel@gmail.com>
2023-05-16 17:45:22 +0500

    change theme default mode by dark

--- src/redux/themeMode/slice.ts
+++ src/redux/themeMode/slice.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ThemeSliceState } from './types';
 
 const initialState: ThemeSliceState = {
-    theme: localStorage.getItem('theme') || 'light'
+    theme: localStorage.getItem('theme') || 'dark'
 };
 
 export const themeSlice = createSlice({
