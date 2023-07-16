9d4c7ec55ae9be111d7a197f3a2c91d2a5a56d0d
Pavel Zolotin <zolotinpavel@gmail.com>
2023-07-16 21:14:45 +0500

    fix single product onclick functionality

--- src/components/ProductBlock/index.tsx
+++ src/components/ProductBlock/index.tsx
@@ -176,15 +176,17 @@ const ProductBlock = ({id, title, imageUrl, descriptionFull, descriptionShort, p
 
     return (
         <Product>
-            <Link to={`/${page}/${id}`}>
-                <ImageBox>
+            <ImageBox>
+                <Link to={`/${page}/${id}`}>
                     <Img
                         src={imageUrl}
                         alt="product"
                     />
-                </ImageBox>
-                <Info>
-                    <TextBox style={!isMobile ? {height: '17rem'} : {}}>
+                </Link>
+            </ImageBox>
+            <Info>
+                <TextBox style={!isMobile ? {height: '17rem'} : {}}>
+                    <Link to={`/${page}/${id}`}>
                         <H3>{title}</H3>
                         {
                             isMobile
@@ -192,34 +194,35 @@ const ProductBlock = ({id, title, imageUrl, descriptionFull, descriptionShort, p
                                 : <H4>{descriptionShort}</H4>
                         }
                         <Span>{weight}</Span>
-                    </TextBox>
-                    <ProductBottom>
-                        <Price>{price} <span>₽</span></Price>
-                        <Button
-                            onClick={() => onClickAdd()}
+                    </Link>
+                </TextBox>
+                <ProductBottom>
+                    <Price>{price} <span>₽</span></Price>
+                    <Button
+                        onClick={() => onClickAdd()}
+                    >
+                        <svg
+                            width="12"
+                            height="12"
+                            viewBox="0 0 12 12"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
                         >
-                            <svg
-                                width="12"
-                                height="12"
-                                viewBox="0 0 12 12"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                                    fill="white"
-                                />
-                            </svg>
-                            <span>Выбрать</span>
-                            {
-                                addedCount > 0 && <i>{addedCount}</i>
-                            }
-                        </Button>
-                    </ProductBottom>
-                </Info>
-            </Link>
+                            <path
+                                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                                fill="white"
+                            />
+                        </svg>
+                        <span>Выбрать</span>
+                        {
+                            addedCount > 0 && <i>{addedCount}</i>
+                        }
+                    </Button>
+                </ProductBottom>
+            </Info>
         </Product>
-    );
+    )
+        ;
 };
 
 export default ProductBlock;
